cf9181ba7a3c8d12d5bf1cfbf964feb7e28b2c4e
peng <dugucanfeng@163.com>
2019-04-17 11:24:26 +0800

    update url

--- src/views/index/index.js
+++ src/views/index/index.js
@@ -28,7 +28,7 @@ export default {
         loop: true,
         controls: true,
         //自动播放属性,muted:静音播放
-        autoplay: "muted",
+        autoplay: true,
         //建议浏览器是否应在<video>加载元素后立即开始下载视频数据。
         preload: "auto",
         //设置视频播放器的显示宽度（以像素为单位）
